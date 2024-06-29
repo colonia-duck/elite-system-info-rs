692c3536b9f5f40e9c0509ba443be491a0ed5050
colonia-duck <tomclarke2006@hotmail.com>
2024-06-29 22:02:16 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/* Using a Spansh Bodies Datadump,
+*  write a Tool that reads a JSON
+*  Stream using a single consumer
+*  and multiple worker threads.
+*  The worker threads look for Planets > 120kls
+*  orbiting a gas giant w/ water based life. 
+*/
+
+// using the flate2 library to stream the file
+
+fn main() {
+    println!("Hello, world!");
+}
