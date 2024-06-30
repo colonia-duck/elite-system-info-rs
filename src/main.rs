3d9916c9cf8b995efde8d755d59ccaddaf2c797a
colonia-duck <tomclarke2006@hotmail.com>
2024-06-30 12:42:03 +0100

    now decompressing the file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,35 @@
 
 // using the flate2 library to stream the file
 
-fn main() {
-    println!("Hello, world!");
+use flate2::read::GzDecoder;
+use serde_json::Value;
+use std::fs::File;
+use std::io::{self, BufReader}; 
+
+fn decoder(file_path: &str) -> io::Result<Vec<u8>> {
+
+    // opens file
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let mut decoder = GzDecoder::new(reader);
+
+    let mut decompressed_data = Vec::new();
+
+    std::io::copy(&mut decoder, &mut decompressed_data);
+
+    Ok(decompressed_data) 
+
+}
+
+fn main() -> io::Result<()>{
+    let data_file = "src/galaxy_7days.json.gz";
+
+    let decompressed_data = decoder(data_file);
+
+    
+
+    println!("parsed data: {:?}", decompressed_data);
+    Ok(())
+    
 }
